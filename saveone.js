ea0181276466b1216f3705c9211f8b3b21388cab
notsaad <42980243+notsaad@users.noreply.github.com>
2018-09-18 14:00:01 -0400

    Final coding complete for crashy bird

--- saveone.js
+++ saveone.js
@@ -1,11 +1,39 @@
-let obstacles = 0
+let ticks = 0
+let emptyObstacleY = 0
+let obstacles: game.LedSprite[] = []
 let bird: game.LedSprite = null
+let index = 0
 input.onButtonPressed(Button.A, () => {
-    bird.change(LedSpriteProperty.Y, 1)
-})
-input.onButtonPressed(Button.B, () => {
     bird.change(LedSpriteProperty.Y, -1)
 })
+input.onButtonPressed(Button.B, () => {
+    bird.change(LedSpriteProperty.Y, 1)
+})
+index = 0
+obstacles = []
 bird = game.createSprite(0, 2)
 bird.set(LedSpriteProperty.Blink, 300)
-obstacles = 0
+basic.forever(() => {
+    while (obstacles.length > 0 && obstacles[0].get(LedSpriteProperty.X) == 0) {
+        obstacles.removeAt(0).delete()
+    }
+    for (let obstacle2 of obstacles) {
+        obstacle2.change(LedSpriteProperty.X, -1)
+    }
+    if (ticks % 3 == 0) {
+        emptyObstacleY = Math.random(5)
+        for (let index2 = 0; index2 <= 4; index2++) {
+            if (index2 != emptyObstacleY) {
+                obstacles.push(game.createSprite(4, index2))
+            }
+        }
+    }
+    for (let obstacle3 of obstacles) {
+        if (obstacle3.get(LedSpriteProperty.X) == bird.get(LedSpriteProperty.X) && obstacle3.get(LedSpriteProperty.Y) == bird.get(LedSpriteProperty.Y)) {
+            game.gameOver()
+        }
+    }
+    ticks += 1
+    basic.pause(1000)
+})
+
